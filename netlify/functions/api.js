29f2810dc423862829ba5239da88af77c994c64e
Deni Shaipov <deni.shaipp@gmail.com>
2023-08-01 23:23:29 +0300

    added cors

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,10 +1,18 @@
 import express, { Router } from 'express';
 import serverless from 'serverless-http';
 
+import cors from 'cors';
+
+const corsOptions = {
+    origin: false,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 const api = express();
+api.use(cors);
 
 const router = Router();
-router.get('/hello', (req, res) => res.send('Hello World!'));
+router.get('/hello', cors(corsOptions), (req, res) => res.send('Hello World!'));
 
 api.use('/api/', router);
 
